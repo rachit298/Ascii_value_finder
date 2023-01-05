1861c78b85d06a223d071ef047ca70ac958690f6
Rachit Srivastava <69860596+rachit298@users.noreply.github.com>
2023-01-06 02:39:48 +0530

    Update asciiValueFinder.js

--- asciiValueFinder.js
+++ asciiValueFinder.js
@@ -2,7 +2,8 @@ const value = document.querySelector("span");
 const keyBoardValue = document.querySelector("kbd");
 const inputVal = document.getElementById("inputVal");
 
-document.addEventListener("keydown", function (event) {
+document.addEventListener("keyup", function (event) {
+  
   const key = event.key;
   const code = key.charCodeAt(0);
   value.textContent = code;
